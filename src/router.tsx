2ef5984f27cdd706f3a2538001cb7a20596f7773
Fichaya <phemmy09@gmail.com>
2020-06-26 03:52:29 +0100

    setup route

---
+++ src/router.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createStackNavigator,
+  TransitionPresets
+} from '@react-navigation/stack';
+import { useTheme } from './theme';
+import Screens from './screens';
+
+import HomeNavigator from './navigator/HomeNavigator';
+import { RootStackParamList } from './screens/types';
+
+const RootStack = createStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  const { fonts, colors } = useTheme();
+
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator
+        initialRouteName="HomeScreen"
+        screenOptions={{
+          headerShown: false,
+          headerBackTitleStyle: {
+            fontFamily: fonts.CORE_SANS_REGULAR,
+            color: colors.BLACK_FONT_COLOR,
+            fontSize: fonts.MEDIUM_SIZE,
+            textTransform: 'capitalize'
+          }
+        }}
+      >
+        <RootStack.Screen
+          name="HomeScreen"
+          component={HomeNavigator}
+          options={{
+            ...TransitionPresets.SlideFromRightIOS
+          }}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+}
