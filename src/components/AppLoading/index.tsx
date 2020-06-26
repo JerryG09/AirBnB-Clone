2c89a5bab2f6af9f0fa0686a5e8dc8bcee2d5b9d
Fichaya <phemmy09@gmail.com>
2020-06-26 03:54:03 +0100

    added Apploading component

---
+++ src/components/AppLoading/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { AppLoading as ExpoAppLoading } from 'expo';
+import { Image } from 'react-native';
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+
+type FontType = { [name: string]: Font.FontSource };
+type AppLoadingProps = { setIsAppReady(T: boolean): void };
+
+export default function AppLoading({ setIsAppReady }: AppLoadingProps) {
+  SplashScreen.preventAutoHideAsync();
+
+  const cacheImages = (images: number[]) => {
+    return images.map((image) => {
+      return typeof image === 'string'
+        ? Image.prefetch(image)
+        : Asset.fromModule(image).downloadAsync();
+    });
+  };
+
+  const cacheFonts = (fonts: FontType[]) => {
+    return fonts.map((font) => Font.loadAsync(font));
+  };
+
+  const loadAllAppAssets = async () => {
+    const imageAssets = cacheImages([
+      require('../../../assets/icon.png'),
+      require('../../../assets/splash.png'),
+    ]);
+
+    const fontAssets = cacheFonts([
+      {
+        'core-sans-regular': require('../../../assets/fonts/CoreSansDSRegular.ttf')
+      },
+      {
+        'core-sans-medium': require('../../../assets/fonts/CoreSansDSMedium.ttf')
+      },
+      {
+        'core-sans-bold': require('../../../assets/fonts/CoreSansDSBold.ttf')
+      }
+    ]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  };
+
+  const handleComplete = () => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+      setIsAppReady(true);
+    }, 500);
+  };
+
+  return (
+    <ExpoAppLoading
+      startAsync={loadAllAppAssets}
+      onFinish={handleComplete}
+      onError={console.warn}
+    />
+  );
+}
